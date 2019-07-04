3e1c1ee6286234cb0cbf4e8f90b7b6c03a051e6e
Kirk <blowme@cow.com>
2019-07-04 12:10:30 -0600

    fix version level

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
