e45fe6afb4c460a520d4e4894ea8ad073e9823d3
Kirk <blowme@cow.com>
2019-07-04 10:15:27 -0600

    production go

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.9',
+    version='0.8.0',
 
     description='SparkFun Electronics qwiic Micro OLED package',
     long_description=long_description,
@@ -63,7 +63,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
