dfbc4d9467d5c7080833c91e4c35e98952bd313e
Kirk <blowme@cow.com>
2019-07-13 12:50:30 -0600

    initial conversion to pythonic style + updates from pylint

--- qwiic_micro_oled/__init__.py
+++ qwiic_micro_oled/__init__.py
@@ -5,7 +5,7 @@
 #
 #
 # Written by  SparkFun Electronics, May 2019
-# 
+#
 #
 # More information on qwiic is at https://www.sparkfun.com/qwiic
 #
@@ -14,28 +14,36 @@
 #==================================================================================
 # Copyright (c) 2019 SparkFun Electronics
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all 
+# The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #==================================================================================
 #-----------------------------------------------------------------------------
 # this package encapsulates the resources used for the Micro OLED board
 #
 #-----------------------------------------------------------------------------
+
+#
+# This is mostly a port of existing Arduino functionaly, so pylint is sad.
+# The goal is to keep the public interface pthonic, but internal is internal
+#
+# pylint: disable=line-too-long
+#
+
 """
 qwiic_micro_oled
 =================
@@ -51,4 +59,4 @@ New to qwiic? Take a look at the entire [SparkFun qwiic ecosystem](https://www.s
 
 
 
-from .qwiic_micro_oled 		import QwiicMicroOled
+from .qwiic_micro_oled      import QwiicMicroOled
